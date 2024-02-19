2366b2056df2a47f6e993536e88efe1a51b8fe78
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-19 12:26:27 +0900

    バトルシステムのシーンからの切り離し、汎用イメージアニメーションスクリプトを作成し、バトルスタートのアニメーションとして設定しました。

---
+++ MagicBullet/Assets/ImageAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageAnimation : MonoBehaviour
+{
+    [SerializeField] private float AnimationSpeed;
+
+    private void Start()
+    {
+        StartAniamtion(AnimationSpeed);
+    }
+
+    public void StartAniamtion(float animationSpeed)
+    {
+        StartCoroutine(ZeroToOneAnimation(animationSpeed));
+    }
+
+    public void EndAnimation()
+    {
+        
+    }
+
+    IEnumerator ZeroToOneAnimation(float animationSpeed)
+    {
+        Image myImage = this.GetComponent<Image>();
+
+        myImage.fillAmount = 0;
+
+        while (myImage.fillAmount < 1)
+        {
+            myImage.fillAmount += animationSpeed * Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("I—¹I");
+    }
+}
