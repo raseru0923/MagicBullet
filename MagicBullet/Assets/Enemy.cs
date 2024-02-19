9307a82ec8d327c56000d433a8c160fd6b3bfa26
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-19 13:42:21 +0900

    エネミースクリプトを追加しました。

---
+++ MagicBullet/Assets/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // “G‚Ìƒ^[ƒ“‚Ìˆ—
+    public IEnumerator EnemyTurn()
+    {
+        yield break;
+    }
+}
