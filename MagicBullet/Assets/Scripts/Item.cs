1d47a1830697e4e4950cee33cec9798a6478ec9f
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-21 11:53:34 +0900

    Itemのデータ構造を作成、Inspector拡張も実施。

--- MagicBullet/Assets/Scripts/Item.cs
+++ MagicBullet/Assets/Scripts/Item.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ItemScriptableObject ItemManager;
+    [System.NonSerialized] public int SelectItem;
+}
 
-    // Update is called once per frame
-    void Update()
+#if UNITY_EDITOR
+[CustomEditor(typeof(Item))]
+public class EditorButtonManager : Editor
+{
+    public override void OnInspectorGUI()
     {
-        
+        base.OnInspectorGUI();
+
+        Item manager = (Item)this.target;
+        // 未セットの場合はnullがありえる
+        if (manager.ItemManager.ItemData == null)
+        {
+            return;
+        }
+
+        EditorGUILayout.LabelField("自身アイテムを指定▼");
+        manager.SelectItem = EditorGUILayout.Popup(
+            manager.SelectItem,
+            manager.ItemManager.ItemData.Select((x) => x.Name).ToArray()
+            );
     }
 }
+#endif
