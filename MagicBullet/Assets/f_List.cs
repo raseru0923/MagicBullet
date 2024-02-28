33ec9eb30fdf8780176a53560e470f5231e6327c
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-28 16:16:09 +0900

    リスト生成プログラムを作りました。

---
+++ MagicBullet/Assets/f_List.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class f_List : MonoBehaviour
+{
+    // ノードの設計図
+    [SerializeField] GameObject NodePrefab;
+    // ノードを並べる場所
+    [SerializeField] GameObject Content;
+
+    public void CreateList(List<string> nodeNames)
+    {
+        for (int i = 0; i < Content.transform.childCount; i++)
+        {
+            GameObject childObject = Content.transform.GetChild(i).gameObject;
+            if (childObject.tag == "Node")
+            {
+                Destroy(Content.transform.GetChild(i).gameObject);
+            }
+        }
+
+        foreach (var item in nodeNames)
+        {
+            StartCoroutine(CreateNode(item));
+        }
+    }
+
+    public void CreateList(string[] nodeNames)
+    {
+        CreateList(nodeNames.ToList());
+    }
+
+    IEnumerator CreateNode(string nodeName)
+    {
+        // ノードを作る
+        GameObject nodeObject = Instantiate(NodePrefab);
+        f_Node node = nodeObject.GetComponent<f_Node>();
+
+        // 親にContentを指定
+        nodeObject.transform.SetParent(Content.transform);
+
+        yield return null;
+
+        // ノードにテキストを設定
+        node.SetLabel(nodeName);
+    }
+}
