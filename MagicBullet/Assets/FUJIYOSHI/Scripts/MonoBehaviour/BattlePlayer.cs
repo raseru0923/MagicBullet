1bd0e2195d2841438fbce9aadd59a4d30daa6367
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-29 14:02:50 +0900

    feat: プレイヤーのターン開始時にコマンドをアクティブにできるようにするため、コマンドのアクティブ切替を行う関数とコマンドオブジェクトのフィールドを追加しました。

--- MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
+++ MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 戦闘時のプレイヤー
 public class BattlePlayer : MonoBehaviour
 {
+    [Header("戦闘用コマンドを設定▼")]
+    [SerializeField] private GameObject BattleCommand;
+
+    // 戦闘用コマンドの動作状態切替
+    // 引数1:動作しているか。
+    public void SetBattleCommandActive(bool isActive)
+    {
+        BattleCommand.SetActive(isActive);
+    }
 }
