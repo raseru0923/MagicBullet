0b9955c3373e2b46ac70b6e1e319956ee4e90e91
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-29 13:48:38 +0900

    refactor: クラスの多重宣言を防ぐために、f_を接頭につけていましたが、不要なのでクラス名からの予測のしやすさのため、名前を変更

--- MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
+++ MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class f_BattlePlayer : MonoBehaviour
+public class BattlePlayer : MonoBehaviour
 {
     [SerializeField, TextArea] private string text;
 }
