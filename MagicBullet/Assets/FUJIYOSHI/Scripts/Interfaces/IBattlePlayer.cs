290f3c49208af8cc98051195de4db67386c31b79
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-02-29 14:32:54 +0900

    feat: キャラクターの追加に対する柔軟性を高めたターン性バトルを実現するため、バトルプレイヤー用のインターフェースを追加し、コマンドのアクティブ切替の関数を宣言、プレイヤースクリプトに継承。

---
+++ MagicBullet/Assets/FUJIYOSHI/Scripts/Interfaces/IBattlePlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBattlePlayer
+{
+    // 戦闘用コマンドの動作状態切替
+    // 引数1:動作しているか。
+    public void SetBattleCommandActive(bool isActive);
+}
--- MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
+++ MagicBullet/Assets/FUJIYOSHI/Scripts/MonoBehaviour/BattlePlayer.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 戦闘時のプレイヤー
-public class BattlePlayer : MonoBehaviour
+public class BattlePlayer : MonoBehaviour, IBattlePlayer
 {
     [Header("戦闘用コマンドを設定▼")]
     [SerializeField] private GameObject BattleCommand;
 
-    // 戦闘用コマンドの動作状態切替
-    // 引数1:動作しているか。
     public void SetBattleCommandActive(bool isActive)
     {
         BattleCommand.SetActive(isActive);
