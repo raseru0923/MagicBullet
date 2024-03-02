06bcf6159375f92ac6850d08c58481d807df5ae3
DESKTOP-67MT2K8\user <bellfe5aloftaida@gmail.com>
2024-03-02 16:54:31 +0900

    feat: ドアの自然な開閉を再現するため、開いた音、閉じた音、Fキーで開く、少し離れると閉じる処理を追加しました。

--- MagicBullet/Assets/OpenDoor.cs
+++ MagicBullet/Assets/OpenDoor.cs
@@ -6,15 +6,20 @@ public class OpenDoor : MonoBehaviour
 {
     private bool canOpen;
     private Animator animator;
+    private AudioSource audioSource;
+
+    [SerializeField] AudioClip OpenClip;
+    [SerializeField] AudioClip CloseClip;
 
     private void OnEnable()
     {
         animator = this.GetComponent<Animator>();
+        audioSource = Camera.main.GetComponent<AudioSource>();
     }
 
     private void Update()
     {
-        if (canOpen && !animator.GetBool("IsOpen"))
+        if (canOpen && !animator.GetBool("IsOpen") && Input.GetKeyDown(KeyCode.F))
         {
             animator.SetBool("IsOpen", true);
         }
@@ -37,5 +42,15 @@ public class OpenDoor : MonoBehaviour
         {
             canOpen = false;
         }
+    }
+
+    public void PlayOpenAudio()
+    {
+        audioSource.PlayOneShot(OpenClip);
+    }
+    
+    public void PlayCloseAudio()
+    {
+        audioSource.PlayOneShot(CloseClip);
     }
 }
